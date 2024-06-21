71b953824188bdeaba9397638e0dafc50bd39e98
Stanislau Marudau <marudov.stas@gmail.com>
2024-06-21 15:56:37 +0300

    feat: implement Meal Component

--- src/app/pages/homepage/homepage.module.ts
+++ src/app/pages/homepage/homepage.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage.component';
+import { MealComponent } from './components/meal/meal.component';
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, MealComponent],
   imports: [CommonModule, HomepageRoutingModule],
   exports: [HomepageComponent],
 })
