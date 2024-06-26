513d7bb56a715173194a0901b7fbdc657ccd5ff9
Stanislau Marudau <marudov.stas@gmail.com>
2024-06-26 14:39:38 +0300

    feat: custom pipe and date pipe implementation

--- src/app/pages/fridge/fridge.module.ts
+++ src/app/pages/fridge/fridge.module.ts
@@ -5,9 +5,10 @@ import { AddProductComponent } from './components/add-product/add-product.compon
 import { FridgeComponent } from './fridge.component';
 import { ProductsTableComponent } from './components/products-table/products-table.component';
 import { FridgeRoutingModule } from './fridge-routing.module';
+import { StringToDatePipe } from './pipes/string-to-date.pipe';
 
 @NgModule({
-  declarations: [FridgeComponent, AddProductComponent, ProductsTableComponent],
+  declarations: [FridgeComponent, AddProductComponent, ProductsTableComponent, StringToDatePipe],
   imports: [CommonModule, FridgeRoutingModule],
   exports: [FridgeComponent],
 })
