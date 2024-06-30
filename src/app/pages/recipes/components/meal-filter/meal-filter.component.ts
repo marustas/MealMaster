e7bce924e32e775f4c90d7a0ce4a1b63d871f7e3
Stanislau Marudau <marudov.stas@gmail.com>
2024-06-30 20:07:00 +0300

    feat: implement meal-filters layout

--- src/app/pages/recipes/components/meal-filter/meal-filter.component.ts
+++ src/app/pages/recipes/components/meal-filter/meal-filter.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-meal-filter',
   templateUrl: './meal-filter.component.html',
   styleUrls: ['./meal-filter.component.scss'],
 })
-export class MealFilterComponent {}
+export class MealFilterComponent {
+  @Output() filterChanged = new EventEmitter<string[]>();
+
+  filters: { [key: string]: boolean } = {
+    breakfast: false,
+    lunch: false,
+    dinner: false,
+  };
+
+  onCheckboxChange(filter: string, isChecked: Event): void {
+    this.filters[filter] = (isChecked.target as HTMLInputElement).checked;
+    const activeFilters = Object.keys(this.filters).filter((key) => this.filters[key]);
+    this.filterChanged.emit(activeFilters);
+  }
+}
