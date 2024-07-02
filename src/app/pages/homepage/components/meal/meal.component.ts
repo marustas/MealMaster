267a01027357dbbe396c8d549fcd61e6e016439b
Stanislau Marudau <marudov.stas@gmail.com>
2024-07-02 16:58:29 +0300

    feat: change recipe functionality implemented

--- src/app/pages/homepage/components/meal/meal.component.ts
+++ src/app/pages/homepage/components/meal/meal.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { IRecipe } from 'src/app/models/IRecipe';
 
 import { MealService } from '../../services/meal.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-meal[meal]',
@@ -12,10 +13,17 @@ export class MealComponent {
   @Input() meal!: IRecipe;
   isModalVisible = false;
 
-  constructor(private mealService: MealService) {}
+  constructor(
+    private mealService: MealService,
+    private router: Router
+  ) {}
 
   onDeleteMeal(): void {
     this.mealService.deleteMeal(this.meal.id);
+  }
+
+  onEditMeal(): void {
+    this.router.navigate(['/recipes', this.meal.section, this.meal.id]);
   }
 
   showModal(): void {
