515ec9e3230ce6791a4df52a519080d5158f4f47
Stanislau Marudau <marudov.stas@gmail.com>
2024-07-04 13:35:20 +0300

    feat: implement email validation

--- src/app/pages/login/components/login-form/login-form.component.ts
+++ src/app/pages/login/components/login-form/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { passwordValidator } from '../../validators/password.validator';
+import { emailValidator } from '../../validators/email.validator';
 
 @Component({
   selector: 'app-login-form',
@@ -12,13 +13,17 @@ export class LoginFormComponent {
 
   constructor(formBuilder: FormBuilder) {
     this.loginForm = formBuilder.group({
-      email: ['', [Validators.required]],
+      email: ['', [Validators.required, emailValidator()]],
       password: ['', [Validators.required, passwordValidator()]],
     });
   }
 
   get passwordControl() {
     return this.loginForm.get('password');
+  }
+
+  get emailControl() {
+    return this.loginForm.get('email');
   }
 
   handleLogin(e: SubmitEvent) {
---
+++ src/app/pages/login/validators/email.validator.ts
@@ -0,0 +1,11 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function emailValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const { value } = control;
+    if (!value) return null;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const valid = emailRegex.test(control.value);
+    return valid ? null : { invalidEmail: { message: 'Invalid email format. Example: name@example.com' } };
+  };
+}
