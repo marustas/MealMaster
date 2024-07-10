68027e1accb277f49a0b131cdb014f4d8cabb97a
Stanislau Marudau <marudov.stas@gmail.com>
2024-07-10 22:01:50 +0300

    test(add-prduct-component) buildPRoduct method

--- src/app/pages/fridge/components/add-product/add-product.component.spec.ts
+++ src/app/pages/fridge/components/add-product/add-product.component.spec.ts
@@ -1,15 +1,18 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 import { ProductsService } from '../../services/products.service';
 import { AddProductComponent } from './add-product.component';
-import { HttpClientModule } from '@angular/common/http';
+import { ProductUnitComponent } from '../product-unit/product-unit.component';
 
 class MockProductService {
   currentLength = 100;
+  addProduct = jest.fn();
+  changeProduct = jest.fn();
+  getProduct = jest.fn().mockReturnValue(of({ name: 'Test Product', expiresAt: '', quantity: '100 g' }));
 }
 
 describe('AddProductComponent', () => {
@@ -19,16 +22,22 @@ describe('AddProductComponent', () => {
   let mockProductService: MockProductService;
 
   beforeEach(async () => {
+    mockProductService = new MockProductService();
+
     await TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule, HttpClientModule],
-      declarations: [AddProductComponent],
-      providers: [{ provide: ProductsService, useClass: MockProductService }],
-      schemas: [NO_ERRORS_SCHEMA],
+      imports: [ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule],
+      declarations: [AddProductComponent, ProductUnitComponent],
+      providers: [FormBuilder, { provide: ProductsService, useValue: mockProductService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddProductComponent);
     component = fixture.componentInstance;
     formBuilder = TestBed.inject(FormBuilder);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
   it('should create expiration date if none provided', () => {
@@ -44,7 +53,7 @@ describe('AddProductComponent', () => {
     expect(product.name).toBe('Test Product');
     expect(product.quantity).toBe('100 g');
     expect(product.expiresAt).toMatch(/\d{1,2}\/\d{1,2}\/\d{4}/);
-    expect(product.id).toBeGreaterThanOrEqual(mockProductService?.currentLength);
+    expect(product.id).toBeGreaterThanOrEqual(mockProductService.currentLength);
   });
 
   it('should keep the productID if provided', () => {
